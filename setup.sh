e01f2c494343153acff55e8bbf1354e1ecabde5f
stanley <114278820+stanleyedward@users.noreply.github.com>
2025-05-04 12:54:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ conda activate python
 conda install conda-forge::gxx=9.5.0
 conda install cuda -c nvidia/label/cuda-12.4.1
 pip3 install torch torchvision torchaudio black
+### torch and torch audio==2.6.0 torchvision==0.21.0
 
 sudo pacman -S \
   base-devel \
@@ -19,3 +20,8 @@ sudo pacman -S \
   lib32-glm
 
 pip install --no-build-isolation submodules/simple-knn submodules/diff-gaussian-rasterization submodules/fused_ssim
+
+### FOR gsplat repo only
+pip install ninja numpy jaxtyping rich wheel packaging
+pip install -r examples/requirements.txt
+
